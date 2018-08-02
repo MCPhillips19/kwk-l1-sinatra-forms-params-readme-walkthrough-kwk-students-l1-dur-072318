1b391c8e1b450f0f9ba8924d71d1b77a96473306
MCPhillips19 <mpclaire72@gmail.com>
2018-08-02 18:34:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
+  get '/dogs' do
+    erb :dogs_form
   end
 
   # Add your post route and action below
-
+post '/dogs' do
+  @name=params[:name]
+  @favorite_dog=params[:favorite_dog]
+  "My name is #{@name}, and I want a #{@favorite_dog}"
+  erb :dogs 
+end
 end
